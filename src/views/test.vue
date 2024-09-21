3604b670bbf40578b2a72d84930580c5522f7002
aziz abd <aziz.abdullaev@proton.me>
2024-09-21 22:43:51 +0500

    feat: play audio

--- src/views/test.vue
+++ src/views/test.vue
@@ -5,6 +5,7 @@
     <p v-if="isRecording">Recording...</p>
     <p v-if="isSending">Sending audio...</p>
     <p v-if="result">Result: {{ result }}</p>
+    <audio ref="audioPlayer" controls v-if="audioSrc"></audio>
   </div>
 </template>
 
@@ -17,6 +18,8 @@ const audioChunks = ref<Blob[]>([])
 const isRecording = ref<boolean>(false)
 const isSending = ref<boolean>(false)
 const result = ref<string | null>(null)
+const audioSrc = ref(null);
+const audioPlayer = ref();
 
 const startRecording = () => {
   isRecording.value = true
@@ -55,24 +58,36 @@ const sendAudioToServer = () => {
 
   const reader = new FileReader()
   reader.readAsDataURL(audioBlob)
-  reader.onloadend = () => {
+  reader.onloadend = async () => {
     const base64Audio = (reader.result as string).split(',')[1] // Remove the "data:audio/wav;base64," part
 
     // This is a dummy UUID from our local db, dont be fooled :))
     const conversation_id = "d168d9e1-225d-43a6-a761-e7150142edf2"
 
-    axios
-      .post(`http://localhost:8000/tts/client_speech_text/${conversation_id}/`, { text: base64Audio })
-      .then((_response) => {
-        isSending.value = false
-        // TODO: input is a file
-        result.value = _response.data.text
-      })
-      .catch((error: Error) => {
-        console.error('Error sending audio:', error)
-        result.value = 'Error sending audio'
-        isSending.value = false
-      })
+    try {
+      const response = await axios
+        .post(`http://localhost:8000/tts/client_speech_text/${conversation_id}/`, { text: base64Audio }, {
+          responseType: 'arraybuffer'
+        })
+
+      const audioBlob = new Blob([response.data], { type: 'audio/wav' });
+
+      audioSrc.value = URL.createObjectURL(audioBlob);
+      isSending.value = false
+
+      if (audioPlayer.value) {
+        audioPlayer.value.load()
+      }
+
+
+    } catch (error) {
+      console.error('Error sending audio:', error)
+      result.value = 'Error sending audio'
+      isSending.value = false
+    }
+
+
+
   }
 }
 </script>
