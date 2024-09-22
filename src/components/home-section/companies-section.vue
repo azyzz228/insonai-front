3c8554cc648ae1beb347c551df6984f7d0e33a5f
Akhmatkulove <timur.akhmatkulov@gmail.com>
2024-09-22 06:43:42 +0300

    minor changes

---
+++ src/components/home-section/companies-section.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div
+      class="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5"
+    >
+      <img
+        class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+        src="@/assets/images/companies/uzum.png"
+        alt="uzum"
+        width="158"
+        height="48"
+      />
+      <img
+        class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+        src="@/assets/images/companies/ipak.webp"
+        alt="ipak"
+        width="158"
+        height="48"
+      />
+      <img
+        class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+        src="@/assets/images/companies/gross.webp"
+        alt="gross insurance"
+        width="158"
+        height="48"
+      />
+      <img
+        class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+        src="@/assets/images/companies/akfa.webp"
+        alt="akfa medline"
+        width="158"
+        height="48"
+      />
+      <img
+        class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+        src="@/assets/images/companies/payme.webp"
+        alt="payme"
+        width="158"
+        height="48"
+      />
+    </div>
+    <div class="mt-16 flex justify-center">
+      <p
+        class="relative rounded-full px-4 py-1.5 text-sm leading-6 text-gray-600 ring-1 ring-inset ring-gray-900/10 hover:ring-gray-900/20"
+      >
+        <span class="hidden md:inline"
+          >Uzum saves up to 48,000,000 UZS per year, per employee by working with us.</span
+        >
+        <a href="#" class="font-semibold text-indigo-600"
+          ><span class="absolute inset-0" aria-hidden="true" /> Read our case study
+          <span aria-hidden="true">&rarr;</span></a
+        >
+      </p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
---
+++ src/components/home-section/feature-section.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="mx-auto mt-32 max-w-7xl px-6 sm:mt-56 lg:px-8">
+    <div class="mx-auto max-w-2xl lg:text-center">
+      <h2 class="text-base font-semibold leading-7 text-indigo-600">Automate now</h2>
+      <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        What is special about INSON AI
+      </p>
+      <p class="mt-6 text-lg leading-8 text-gray-600">
+        INSON AI is the next-generation call center automation tool that handles customer
+        interactions, leads processing, feedback collection, booking confirmations, and more—all in
+        real time.
+      </p>
+    </div>
+    <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+      <dl
+        class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16"
+      >
+        <div v-for="feature in features" :key="feature.name" class="relative pl-16">
+          <dt class="text-base font-semibold leading-7 text-gray-900">
+            <div
+              class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600"
+            >
+              <component :is="feature.icon" class="h-6 w-6 text-white" aria-hidden="true" />
+            </div>
+            {{ feature.name }}
+          </dt>
+          <dd class="mt-2 text-base leading-7 text-gray-600">{{ feature.description }}</dd>
+        </div>
+      </dl>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import {
+  ArrowPathIcon,
+  BanknotesIcon,
+  ClipboardDocumentCheckIcon,
+  CodeBracketSquareIcon,
+  LanguageIcon,
+  PhoneIcon
+} from '@heroicons/vue/24/outline'
+
+const features = [
+  {
+    name: 'Real-Time Call Automation',
+    description:
+      'INSON AI handles entire customer calls, from greeting to resolution, ensuring consistent and effective interactions.',
+    icon: PhoneIcon
+  },
+  {
+    name: 'Lead Processing & Follow-Up',
+    description:
+      'Capture, process, and nurture leads automatically, allowing your team to focus on closing deals.',
+    icon: BanknotesIcon
+  },
+  {
+    name: 'Feedback & Quality Assurance',
+    description: 'Collect valuable customer feedback and perform quality assurance seamlessly.',
+    icon: ArrowPathIcon
+  },
+  {
+    name: 'Booking & Verification',
+    description: 'Confirm appointments and reservations through automated, personalized calls.',
+    icon: ClipboardDocumentCheckIcon
+  },
+  {
+    name: 'Integration with Your CRM',
+    description: 'Add INSON AI into your existing CRM and boost your customer service.',
+    icon: CodeBracketSquareIcon
+  },
+  {
+    name: 'Multi-Language Support',
+    description: 'Communicate with your customers in multiple languages for broader outreach.',
+    icon: LanguageIcon
+  }
+]
+</script>
+
+<style scoped lang="scss"></style>
